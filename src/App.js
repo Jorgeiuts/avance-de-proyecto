d3a73ec2a0b09fcb45fd4249e5256368da3a9f5c
GiovanniVega1 <edwinvega.torres@gmail.com>
2023-05-25 20:09:16 -0600

    nuevo commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Table, Button, Container, Modal, ModalBody, ModalHeader, FormGroup, ModalFooter} from 'reactstrap';
+import { Table, Button, Container, Modal, ModalBody, ModalHeader, FormGroup, ModalFooter, Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem, NavbarText } from 'reactstrap';
 
 
-class App extends React.Component{
-  render(){
-    return(
+class App extends React.Component {
+  render() {
+    const Example = () => {
+      const [isOpen, setIsOpen] = useState(false);
+    
+      const toggle = () => setIsOpen(!isOpen);
+    
+      return (
+        <div>
+          <Navbar expand="md" color="light" light>
+            <NavbarBrand href="/">reactstrap</NavbarBrand>
+            <NavbarToggler onClick={toggle} />
+            <Collapse isOpen={isOpen} navbar>
+              <Nav className="me-auto" navbar>
+                <NavItem>
+                  <NavLink href="/components/">Nosotros</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="https://github.com/reactstrap/reactstrap">
+                    GitHub
+                  </NavLink>
+                </NavItem>
+                <UncontrolledDropdown nav inNavbar>
+                  <DropdownToggle nav caret>
+                    Options
+                  </DropdownToggle>
+                  <DropdownMenu right>
+                    <DropdownItem>Option 1</DropdownItem>
+                    <DropdownItem>Option 2</DropdownItem>
+                    <DropdownItem divider />
+                    <DropdownItem>Reset</DropdownItem>
+                  </DropdownMenu>
+                </UncontrolledDropdown>
+              </Nav>
+              <NavbarText>Simple Text</NavbarText>
+            </Collapse>
+          </Navbar>
+        </div>
+      );
+    };
+  
+    return (
       <>
-      <Button color='primary'>Insertar Nueva Pelicula</Button>
-      </>)
+        <Example />
+        <Button color="primary">Insertar Nueva Pelicula</Button>
+        
+      </>
+    );
   }
 }
 
