2f2380bade3d5e550ad57cff5ec712b4668bcdc8
AlonsoSotelo <cocolisojorge99@gmail.com>
2023-05-25 19:11:11 -0600

    Se instala bootstarp y reactstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Table, Button, Container, Modal, ModalBody, ModalHeader, FormGroup, ModalFooter} from 'reactstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component{
+  render(){
+    return(
+      <>
+      <Button color='primary'>Insertar Nueva Pelicula</Button>
+      </>)
+  }
 }
 
 export default App;
