6b0feec8c9c9343ecfe32cc86d194e11df79c51a
AlonsoSotelo <cocolisojorge99@gmail.com>
2023-05-31 12:02:49 -0600

    eliminacion del logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Table, Button, Container, Modal, ModalBody, ModalHeader, FormGroup, ModalFooter, Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem, NavbarText } from 'reactstrap';
